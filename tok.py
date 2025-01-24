98807fc3087fdf25464a2964c343f16494d2bb5c
Shusrith S <116418999+MBUYt0n@users.noreply.github.com>
2025-01-24 09:32:01 +0530

    pls work god

--- tok.py
+++ tok.py
@@ -1,12 +1,22 @@
 from transformers import BertTokenizer
+import torch
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-with open("data/Ant-Man.txt", "r") as f:
-    text = f.read()
-    print(len(text))
-    print(text[4])
-    t = tokenizer.encode(text, return_tensors="pt")
-    f.close()
 
-print(t.shape)
-print(t[0][4])
+class Tokenize:
+    def __init__(self, corpus):
+        self.corpus = corpus
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+    def tokenize(self):
+        toks = []
+        for i in self.corpus:
+            toks.append(self.tokenizer.encode(i, return_tensors="pt")[0])
+        return toks
+
+    def get_data(self, seq_length):
+        inps = []
+        toks = self.tokenize()
+        for j in toks:
+            for i in range(seq_length, len(j) - 1):
+                inps.append(j[i - seq_length: i + 1])
+        return torch.utils.data.DataLoader(inps, batch_size=32, shuffle=True)
