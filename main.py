63e1e86346e0de92034e277365cc518bb2984d9b
mbuyt0n <shusrith03@gmail.com>
2025-01-26 21:11:26 +0530

    i cant believe it

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 from train_gpu import Trainer
 
 
-l = os.listdir("data")[:2]
-k = os.listdir("data")[3:4]
+l = os.listdir("data")[:3]
+k = os.listdir("data")[4:5]
 valcorpus = []
 for i in k:
     with open("data/" + i, "r") as f:
@@ -25,9 +25,10 @@ t = Tokenize(corpus)
 data = t.get_data(10)
 model = LLM(50257, 100, 4, 4, 768).to("cuda")
 t = Trainer(model)
-t.train(data, 10)
+t.train(data, 20)
 
 a = next(iter(valdata))[0]
 a = a.to("cuda")
 x = model.generate(a.unsqueeze(0), 50)
 print(valt.decode(x))
+torch.save(model.state_dict(), "model.pth")
